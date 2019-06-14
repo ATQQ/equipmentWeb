1956bcc9c9d048890e38ce31b33765994839239c
tandake <1552832259@qq.com>
2019-06-14 20:18:35 +0800

    :rocket:新增部分处理逻辑，优化页面内容

--- static/assets/js/view/renderDeviceData.js
+++ static/assets/js/view/renderDeviceData.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
 
     //mock数据
     /**
-    * @param {*} url 第一个参数 当前请求的api
-    * @param {*} data  第二个参数 需要返回数据的类型设置
+    * @param {string} url 第一个参数 当前请求的api
+    * @param {json} data  第二个参数 需要返回数据的类型设置
     * 返回当前设备的信息
     * @returns {deviceObj} 返回虚拟请求的设备信息
     */
@@ -31,7 +31,7 @@ $(document).ready(function () {
 
     //初始化deviceTable 对象
     var deviceTable = initDataTables();
-
+    
     var catgoryData = new Map();
     /**
      *获取分类的数据
@@ -49,12 +49,30 @@ $(document).ready(function () {
     $('.all-device').on('click', function () {
 
     })
-    //用户点击提交之后 获取表单的值
+    //用户点击提交之后 获取表单的值 改变表格数据 改变上传图片样式 
     $('#add-device-btn').on('click', function (e) {
         e.preventDefault();
         var data = $('#add-device-form').serializeArray();
-        console.log(data);
-        data = formatObj(data);
+        var key = true;
+        
+        //把获取的数组转化为对象 把获取的图片地址剪切进来
+        data = formatObj(data); 
+        //管理员账号获取
+        data.eqAdmin = '黄海';
+        //图片返回
+        data.images = 'assets/images/testImg.png"';
+        //判断用户填写的信息是否符合规范
+        for(prop in data){
+            if(data[prop] == ''){
+                alert("请把设备信息填写完整！！");
+                console.log(data);
+                key = false;
+                break;
+            }
+        }
+        
+        if(true){
+        addDataToDeviceTable(data)}
     })
 
     /**
@@ -75,14 +93,110 @@ $(document).ready(function () {
         }
     })
 
-    //===========================function部分===========================
+    /**
+     * 
+     * 对已经在表中的数据进行编辑
+     */
 
+    $('#deviceTable tbody').on('click','.edit-btn',function(e){
+        //当前操作的行
+        const $row = deviceTable.row($(this).parents('tr'));
+        //待编辑的设备id
+        const deviceId = $($row.data()[10]).attr("deviceId")
+        //待编辑的设备的所有信息
+        const editData = $($row.data());
+        //把数据填充到表单之中
+        renderModalData(editData);
+    })
+
+    /**
+     * 
+     * 处理用户添加分类的操作
+     */
+    $(".category-add-btn").on('click',function(e){
+        e.preventDefault();
+        //获取用户输入的新分类名
+        var addForm = $(".addCategory").val();
+        var key = true;
+        var newCategory = '';
+        //将新分类名与已有的分类名进行比较
+        catgoryData.forEach(function (ele,index){
+            if(ele == addForm.toString()){
+                alert("当前分类已经存在，请输入未添加的分类名！");
+                key = false;
+            }
+        })
+        if(key &&  addForm != ""){
+            newCategory += '<div class="alert alert-info alert-dismissible  pull-left m-r-7" role="alert">\
+            <button type="button" class="close category-del-btn" data-dismiss="" aria-label="">\
+            <span aria-hidden="true">&times;</span></button>\
+            <strong>'+ addForm +'</strong>&nbsp;</div>';
+            $('.taglist').append(newCategory);
+            //ajax 更新数据库 获取id
+            //更新当前页面的分类map的值
+            //改变添加的编辑的表单select          
+            alert("添加成功");
+        }
+        //重置
+        $(".addCategory").val('');
+    })
+    
+    /**
+     * 
+     * 处理用户删除分类的操作
+     */
+    $(".taglist").on('click','.category-del-btn',function(e){
+        //获取当前标签的值
+        var  categoryName = $(this).parent('div').find('strong').text();        
+        //判断当前分类标签是否存在于表格中
+        var len = deviceTable.data().length;
+        var order = 3;
+        var key = true;
+        var $data = deviceTable.data();
+        for(var i = 0 ; i < len ; i++ ){
+            if($data[i][order] == categoryName){
+                alert("删除失败，仍然有当前分类设备在表格中!");
+                key = false;
+                break;
+            }
+        }
+        if(key){
+          if(confirm("确认删除吗？")){
+            $(this).attr('data-dismiss','alert');
+            alert("删除成功!!");
+            //ajax发送到数据库
+            //修改对应的map
+            //改变添加的编辑的表单select限选框
+            (".category-del-btn").trigger('click');
+          }
+        }
+    })
+   
+    //===========================function部分===========================
+    
+    function initCategory(){
+
+    }
+    /** 
+     *
+     *渲染编辑仪器信息时 信息弹窗的数据
+     * @param {Array} data 当前一排的数据信息
+     */
+    function renderModalData(data){
+          var editForm = document.getElementById('edit-device-form');
+          editForm['eqName'].value = data[1];
+          editForm['eqNumber'].value = data[2];
+          editForm['introduce'].value = data[4];
+          editForm['amount'].value = data[5];
+          
+    }      
     /**
      * 向设备表添加一条数据
      * @param {object} data 
      */
     function addDataToDeviceTable(data) {
-        var $btns = '<div deviceId="' + data.eqId + '" style="display:flex;"><button class="success btn btn-info edit-btn">编辑</button>' +
+        var $btns = '<div deviceId="' + data.eqId + '" style="display:flex;"><button type="button" class="btn btn-primary edit-btn" \
+             data-toggle="modal" data-target="#deviceModalEdit">编辑</button>' + 
             '<button class="btn del-btn btn-danger" style="margin-left:10px;"> 删除</button></div>'
         var $img = '<img src="' + data.images + '" style="width:100px;height:100px;">'
         console.log();
@@ -90,7 +204,7 @@ $(document).ready(function () {
             $img,
             data.eqName,
             data.eqNumber,
-            catgoryData.get(data.categoryId % 2),
+            catgoryData.get(data.categoryId % 3),
             data.introduce,
             data.amount,
             data.loan,
@@ -120,7 +234,7 @@ $(document).ready(function () {
         }).done(function (res, status) {
             datas = res.data;
         });
-
+        console.log(datas);
 
         datas.forEach((key) => {
             addDataToDeviceTable(key);
@@ -132,8 +246,9 @@ $(document).ready(function () {
      */
     async function getCateGoryData() {
         //ajax
-        catgoryData.set(1, "相机");
-        catgoryData.set(0, "印象");
+        catgoryData.set(0, "音响");
+        catgoryData.set(1, "相机");    
+        catgoryData.set(2, "录音");
     }
 
     /**
@@ -153,7 +268,7 @@ $(document).ready(function () {
 
 
     /**
-     * @param {*} formArr 用户当前填写的待提交的表单(以类数组的方式传入) 类数组中元素有value 与 name
+     * @param {object} formArr 用户当前填写的待提交的表单(以类数组的方式传入) 类数组中元素有value 与 name
      * 返回当前设备的信息
      * @returns {deviceObj} 返回处理过后的用户填入的表单数据
      */
@@ -164,6 +279,7 @@ $(document).ready(function () {
                 deviceObj[formArr[i].name] = formArr[i].value;
             }
         }
+        
         return deviceObj;
     }
 
@@ -186,7 +302,7 @@ $(document).ready(function () {
         //文件接收服务端接口
         server: baseUrl + "file/save",
         // 选择文件的按钮
-        pick: '#imagePicker',
+        pick: {id:'#imagePicker'},
         //上传请求的方法
         method: "POST",
         // 不压缩image, 默认如果是jpeg，文件上传前会压缩一把再上传！
